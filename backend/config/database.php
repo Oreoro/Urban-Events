e13e1480f771820aada51e251a1ba3963fd53db4
Bilal Arshad <111754659+Oreoro@users.noreply.github.com>
2025-08-13 04:22:40 -0700

    Update database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -2,19 +2,15 @@
 
 use Illuminate\Support\Str;
 
+// This logic for parsing the DATABASE_URL is fine for the pgsql connection.
 $url = env('DATABASE_URL') ? parse_url(env('DATABASE_URL')) : [];
 
-$db =  [
+return [
 
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
     |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
-    |
     */
 
     'default' => env('DB_CONNECTION', 'pgsql'),
@@ -23,16 +19,6 @@ $db =  [
     |--------------------------------------------------------------------------
     | Database Connections
     |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
     */
 
     'connections' => [
@@ -91,8 +77,6 @@ $db =  [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
     ],
@@ -101,11 +85,6 @@ $db =  [
     |--------------------------------------------------------------------------
     | Migration Repository Table
     |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
-    |
     */
 
     'migrations' => 'migrations',
@@ -114,11 +93,6 @@ $db =  [
     |--------------------------------------------------------------------------
     | Redis Databases
     |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
     */
 
     'redis' => [
@@ -127,30 +101,31 @@ $db =  [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'ssl' => true,  // Ensure SSL is set to true
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            // This is the correct way to enable SSL/TLS for Azure Redis
+            'scheme' => 'tls',
+            // This is critical for preventing queue worker timeouts
+            'read_write_timeout' => -1,
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+            // Also enable SSL/TLS here
+            'scheme' => 'tls',
+            // And also add the timeout here
+            'read_write_timeout' => -1,
         ],
 
     ],
 
 ];
-
-return $db;
