4bc9e723141f5124b62707a5b567a7ed421bb271
Bilal Arshad <111754659+Oreoro@users.noreply.github.com>
2025-08-15 23:36:28 -0700

    Update index.tsx
    revert

--- frontend/src/components/modals/AboutModal/index.tsx
+++ frontend/src/components/modals/AboutModal/index.tsx
@@ -1,1 +1,12 @@
+import {Modal} from "../../common/Modal";
+import {GenericModalProps} from "../../../types.ts";
+import classes from "./AboutModal.module.scss";
 
+export const AboutModal = ({onClose}: GenericModalProps) => {
+    return (
+        <Modal onClose={onClose} opened>
+            <div className={classes.aboutContainer}>
+            </div>
+        </Modal>
+    );
+}
