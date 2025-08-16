f42dc89175efd513c79d0905a525ad0ee7c456dc
Bilal Arshad <111754659+Oreoro@users.noreply.github.com>
2025-08-15 23:21:27 -0700

    Update index.tsx

--- frontend/src/components/common/GlobalMenu/index.tsx
+++ frontend/src/components/common/GlobalMenu/index.tsx
@@ -48,14 +48,6 @@ export const GlobalMenu = () => {
         })
     }
 
-    if (!getConfig("VITE_HIDE_ABOUT_LINK")) {
-        links.push({
-            label: `About & Support`,
-            icon: IconLifebuoy,
-            onClick: openAboutModal,
-        });
-    }
-
     links.push({
         label: t`Create Organizer`,
         icon: IconPlus,
