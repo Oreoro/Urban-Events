f2ebf7898a7eb70fe0bea10a924d3a74c5e79ec0
Bilal Arshad <111754659+Oreoro@users.noreply.github.com>
2025-08-20 07:02:06 -0700

    cert add

--- backend/config/database.php
+++ backend/config/database.php
@@ -21,7 +21,8 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'verify-full'),
+            'sslrootcert' => env('DB_SSL_ROOT_CERT', ''),
         
         ],
     ],
