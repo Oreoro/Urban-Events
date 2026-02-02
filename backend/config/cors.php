afd81fb229750b34278c58b03de4573bc9494ea5
Bilal Arshad <111754659+Oreoro@users.noreply.github.com>
2026-02-02 13:48:44 +0500

    Update allowed_origins for CORS configuration

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'https://urbanevents.pk,https://www.urbanevents.pk,https://app.urbanevents.pk')),
 
     'allowed_origins_patterns' => [],
 
