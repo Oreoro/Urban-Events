976620cdfdf60a65666e9242f57bb81c7a5b08b5
Bilal Arshad <111754659+Oreoro@users.noreply.github.com>
2026-07-07 16:05:57 +0500

    Polish shared pagination controls

--- frontend/src/components/common/Pagination/index.tsx
+++ frontend/src/components/common/Pagination/index.tsx
@@ -1,14 +1,23 @@
 import {Box, Pagination as MantinePagination, PaginationProps as MantinePaginationProps} from "@mantine/core";
+import classes from './Pagination.module.scss';
 
 interface PaginationProps extends Omit<MantinePaginationProps, 'hideWithOnePage'> {
     marginTop?: number;
 }
 
 export const Pagination = (props: PaginationProps) => {
-    const {marginTop, ...rest} = props;
+    const {className, marginTop, ...rest} = props;
+
     return (
-        <Box mt={props.marginTop === undefined ? 20 : props.marginTop}>
-            <MantinePagination hideWithOnePage {...rest} />
+        <Box
+            mt={marginTop === undefined ? 14 : marginTop}
+            className={classes.wrapper}
+        >
+            <MantinePagination
+                hideWithOnePage
+                className={`${classes.pagination} ${className || ''}`}
+                {...rest}
+            />
         </Box>
     );
 };
